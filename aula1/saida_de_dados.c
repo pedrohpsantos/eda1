6d75340602386d2ca1ec67a5535b6069d857a796
Pedro Henrique <232038442@aluno.unb.br>
2025-08-20 08:41:24 -0300

    saida_de_dados.c

---
+++ aula1/saida_de_dados.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num = 5;
+    float pi = 3.14159;
+    char letra = 'A';
+    printf("Inteiro: %d\n", num);
+    printf("Ponto Flutuante: %f\n", pi);
+    printf("Caractere: %c\n", letra);
+    return 0;
+} 
