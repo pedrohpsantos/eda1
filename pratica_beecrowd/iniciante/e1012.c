fe9ca6483bb3aaa8bbc41b8179fd260b22988bec
Pedro Henrique <232038442@aluno.unb.br>
2025-08-22 17:09:57 -0300

    Create e1012.c

---
+++ pratica_beecrowd/iniciante/e1012.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h> 
+ 
+int main() {
+ 
+    double a, b, c;
+    double tri, cir, tra, qua, ret, pi = 3.14159;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    tri = (a * c)/2;
+    cir = pi * pow(c, 2);
+    tra = (a + b) * c/2;
+    qua = pow(b, 2);
+    ret = a * b;
+    printf("TRIANGULO: %.3lf\n", tri);
+    printf("CIRCULO: %.3lf\n", cir);
+    printf("TRAPEZIO: %.3lf\n", tra);
+    printf("QUADRADO: %.3lf\n", qua);
+    printf("RETANGULO: %.3lf\n", ret);
+ 
+    return 0;
+}
