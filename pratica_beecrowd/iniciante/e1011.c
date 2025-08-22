2747a8caebf7c4ab5503a98eb6d7cb32a83e2d55
Pedro Henrique <232038442@aluno.unb.br>
2025-08-22 15:24:59 -0300

    Create e1011.c

---
+++ pratica_beecrowd/iniciante/e1011.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+ 
+    double r, circulo, pi = 3.14159;
+    scanf("%lf", &r);
+    circulo = (4.0/3.0)*pi*pow(r, 3);
+    printf("VOLUME = %.3lf\n", circulo);
+ 
+    return 0;
+}
