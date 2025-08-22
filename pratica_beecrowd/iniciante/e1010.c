41909ba317891911d99aac0431df8b5f956ffa22
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-21 23:49:48 -0300

    Add initial implementation of a program to calculate total payment based on item codes and values

---
+++ pratica_beecrowd/iniciante/e1010.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int cod1, cod2, num1, num2;
+    double val1, val2, total;
+
+    scanf("%d %d %lf", &cod1, &num1, &val1);
+    scanf("%d %d %lf", &cod2, &num2, &val2);
+
+    total = (num1 * val1) + (num2 * val2);
+
+    printf("VALOR A PAGAR: R$ %.2lf\n", total);
+
+    return 0;
+}
