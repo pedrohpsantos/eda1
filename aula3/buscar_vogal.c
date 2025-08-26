9779d314a12bf54bfd36c66a54227bb16a07fb83
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-26 17:40:21 -0300

    formatting code

--- aula3/buscar_vogal.c
+++ aula3/buscar_vogal.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int LinearSearch(char vect[], int size, char target){
-    for (int i = 0; i < size; i++){
-        if (vect[i] == target) return i;
+int LinearSearch(char vect[], int size, char target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (vect[i] == target)
+            return i;
     }
     return -1;
 }
 
-int main(){
+int main()
+{
 
     char vowel[5] = {'A', 'E', 'I', 'O', 'U'};
     char letter;
@@ -15,14 +19,14 @@ int main(){
     printf("Digite uma vogal para buscar no vetor: ");
     scanf(" %c", &letter);
     position = LinearSearch(vowel, 5, letter);
-    if (position != -1){
+    if (position != -1)
+    {
         printf("Encontrado na posição: %d", position);
     }
-    else {
+    else
+    {
         printf("Elemento não encontrado");
     }
-
-
 
     return 0;
 }
