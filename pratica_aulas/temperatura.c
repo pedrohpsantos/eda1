c354f818cae1955a64f7d6e73fb3a39b24e3a1ee
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-27 09:31:07 -0300

    add temepratura.c

--- pratica_aulas/temperatura.c
+++ pratica_aulas/temperatura.c
@@ -4,26 +4,36 @@ int main()
 {
 
     char opc;
-    printf("C - Fahrenheit para Celsius, F - Celsius para Fahrenheit e S - Sair");
-    scanf("%c", &opc);
-    switch (opc)
+    do
     {
-    case 'C':
+        printf("C - Fahrenheit para Celsius, F - Celsius para Fahrenheit e S - Sair: ");
+        scanf(" %c", &opc);
+        switch (opc)
+        {
+        case 'C':
+            double f;
+            printf("Digite o valor em Fahrenheit: ");
+            scanf("%lf", &f);
+            printf("Temperatura em Celsius: %.1lf\n", (f - 32) / 1.8);
+            break;
 
-        break;
+        case 'F':
+            double c;
+            printf("Digite o valor em Celsius: ");
+            scanf("%lf", &c);
+            printf("Temperatura em Fahrenheit: %.1lf\n", (c * 1.8) + 32);
+            break;
 
-    case 'F':
+        case 'S':
+            printf("Programa encerrado");
+            break;
 
-        break;
+        default:
+            printf("Opcao invalida");
+            break;
+        }
 
-    case 'S':
-        printf("Programa encerrado");
-        break;
-
-    default:
-        printf("Opcao invalida");
-        break;
-    }
+    } while (opc != 'S');
 
     return 0;
 }
