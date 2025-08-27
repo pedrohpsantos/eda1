81725d8fd0927edfe47493d2dc18b983de640086
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-27 16:26:25 -0300

    add e1013.c

---
+++ pratica_beecrowd/iniciante/e1013.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int MaiorAB(int a, int b)
+{
+    int aux = (a + b + abs(a - b)) / 2;
+    return aux;
+}
+
+int main()
+{
+
+    int a, b, c, maior, aux;
+    scanf("%d %d %d", &a, &b, &c);
+    aux = MaiorAB(a, b);
+    maior = MaiorAB(aux, c);
+    printf("%d eh o maior\n", maior);
+
+    return 0;
+}
