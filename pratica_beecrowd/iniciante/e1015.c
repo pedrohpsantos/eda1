86a77891448a8a316242f116e04de02798ea59c2
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-28 16:08:49 -0300

    add e1015.c

--- pratica_beecrowd/iniciante/e1015.c
+++ pratica_beecrowd/iniciante/e1015.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(){
-
-    
-
+int main()
+{
+    double x1, x2, y1, y2, px, py, media;
+    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x2, &y2);
+    px = x2 - x1;
+    py = y2 - y1;
+    media = sqrt(pow(px, 2) + pow(py, 2));
+    printf("%.4lf\n", media);
     return 0;
 }
