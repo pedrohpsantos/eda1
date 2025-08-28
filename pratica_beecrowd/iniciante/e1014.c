166e363eafacdb354d1d7c2e00803ad6b6b78ff7
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-28 16:00:29 -0300

    add e1014.c

---
+++ pratica_beecrowd/iniciante/e1014.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    double km, l, cons;
+    scanf("%lf", &km);
+    scanf("%lf", &l);
+    cons = km / l;
+    printf("%.3lf km/l\n", cons);
+
+    return 0;
+}
