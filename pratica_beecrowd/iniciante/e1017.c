e57e984ece7d12bf8120df749ed6dc84935ee94e
pedrohpsantos <phps2014.phps@gmail.com>
2025-08-29 15:26:08 -0300

    add e1017.c

---
+++ pratica_beecrowd/iniciante/e1017.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double vm, h, comb;
+    scanf("%lf", &h);
+    scanf("%lf", &vm);
+    comb = (h*vm)/12;
+    printf("%.3lf\n", comb);
+ 
+    return 0;
+}
