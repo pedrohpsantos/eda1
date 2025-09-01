fead6c8f590bb3af315c8c872035e28084f2857d
pedrohpsantos <phps2014.phps@gmail.com>
2025-09-01 16:15:06 -0300

    add e1019.c

---
+++ pratica_beecrowd/iniciante/e1019.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n, h, m, s, resto;
+    scanf("%d", &n);
+    h = n / 3600;
+    resto = n % 3600;
+    m = resto / 60;
+    s = resto % 60;
+    printf("%d : %d : %d\n", h, m, s);
+ 
+    return 0;
+}
