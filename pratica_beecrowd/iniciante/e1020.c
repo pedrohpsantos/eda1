f7bc4ab2549d02bbf6b16cdbf3cdba22dd2cd618
pedrohpsantos <phps2014.phps@gmail.com>
2025-09-15 16:29:04 -0300

    add 1020c

---
+++ pratica_beecrowd/iniciante/e1020.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int idade, ano, mes, resto, dia;
+
+    scanf("%d", &idade);
+
+    ano = idade / 365;
+    resto = idade % 365;
+    mes = resto / 30;
+    dia = resto % 30;
+
+    printf("%d ano(s)\n", ano);
+    printf("%d mes(es)\n", mes);
+    printf("%d dia(s)\n", dia);
+
+    return 0;
+}
