039c04de61120252156c64f699557ef413092b70
pedrohpsantos <phps2014.phps@gmail.com>
2025-09-18 16:30:28 -0300

    add e1037.c

--- pratica_beecrowd/iniciante/selecao/e1037.c
+++ pratica_beecrowd/iniciante/selecao/e1037.c
@@ -11,19 +11,19 @@ int main()
     }
     else if (valor > 25 && valor <= 50)
     {
-        printf("Intervalo (25,50]");
+        printf("Intervalo (25,50]\n");
     }
     else if (valor > 50 && valor <= 75)
     {
-        printf("Intervalo (50,75]");
+        printf("Intervalo (50,75]\n");
     }
     else if (valor > 75 && valor <= 100)
     {
-        printf("Intervalo (75,100]");
+        printf("Intervalo (75,100]\n");
     }
     else
     {
-        printf("Fora de intervalo");
+        printf("Fora de intervalo\n");
     }
 
     return 0;
