2cfefd7248e2fcc6283fe8f3d9def1365a2e18d7
pedrohpsantos <phps2014.phps@gmail.com>
2025-09-23 17:11:33 -0300

    add e1041.c + e1042.c

---
+++ pratica_beecrowd/iniciante/selecao/e1042.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int x=a, y=b, z=c, temp;
+    if(x > y){
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    if(x > z){
+        temp = x;
+        x = z;
+        z = temp;
+    }
+    if (y > z){
+        temp = y;
+        y = z;
+        z = temp;
+    }
+    printf("%d\n%d\n%d\n", x, y, z);
+    printf("\n%d\n%d\n%d\n", a, b, c);
+
+    return 0;
+}
