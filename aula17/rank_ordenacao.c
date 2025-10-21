6982a1e356a18bf56021e00bd0b7d639c065e113
pedrohpsantos <phps2014.phps@gmail.com>
2025-10-21 17:42:43 -0300

    formatting

--- aula17/rank_ordenacao.c
+++ aula17/rank_ordenacao.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include "ordenacoes.h"
+
+int main()
+{
+    FILE *fp = fopen("estimativa_2021.csv", "r");
+    if (!fp)
+    {
+        printf("Erro\n");
+        return 1;
+    }
+    
+    return 0;
+}
